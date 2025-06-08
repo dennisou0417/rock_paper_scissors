430f7c5004ffd487b4077a98534538d5294721f5
dennisou0417 <dennis.ou0417@gmail.com>
2025-06-07 20:50:29 -0400

    playRound() created

--- script.js
+++ script.js
@@ -2,20 +2,16 @@ function getComputerChoice(){
     let choice = Math.ceil(Math.random()*3);
 
     if(choice === 1){
-        return console.log("Rock");
+        return "Rock";
     }else if(choice === 2){
-        return console.log("Paper");
+        return "Paper";
     }else{
-        return console.log("Scissors");
+        return "Scissor";
     }
 }
 
-getComputerChoice();
-
 function getHumanChoice(){
-    let choice = prompt("Rock Paper or Scissors");
-
-    console.log(choice);
+    let choice = prompt("Rock Paper or Scissor");
 
     return choice;
 }
@@ -25,7 +21,28 @@ let computerScore = 0;
 
 function playRound(computerChoice, humanChoice){
     humanChoice = getHumanChoice().toLowerCase();
-    console.log(humanChoice);
+    computerChoice = computerChoice.toLowerCase();
+
+    console.log(humanChoice + " " + computerChoice);
+    
+    if(humanChoice === "scissor" && computerChoice === "paper"){
+        humanScore += 1;
+        console.log("You Win! Scissor cut paper");
+        console.log("Score: YOU: " + humanScore + " AI: " + computerScore);
+    }else if(humanChoice === "paper" && computerChoice === "rock"){
+        humanScore += 1;
+        console.log("You Win! Paper beats rock");
+        console.log("Score: YOU: " + humanScore + " AI: " + computerScore);
+    }else if(humanChoice === "rock" && computerChoice ==="scissor"){
+        humanScore += 1;
+        console.log("You Win! Rock smashes scissor");
+        console.log("Score: YOU: " + humanScore + " AI: " + computerScore);
+    }else if(humanChoice === computerChoice){
+        console.log("Play Again");
+        playRound(getComputerChoice(), getHumanChoice);
+    }else{
+        console.log("You Lose, " + computerChoice + " beats " + humanChoice);
+    }
 }
 
-playRound();
+playRound(getComputerChoice(), getHumanChoice);
