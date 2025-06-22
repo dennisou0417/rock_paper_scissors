e5099986a576d7d12a8c5c29feba4221ecb26783
dennisou0417 <dennis.ou0417@gmail.com>
2025-06-22 01:13:25 -0400

    implementing 5 round games with more UI addition.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const rockBtn = document.querySelector("#rockBtn");
 const paperBtn = document.querySelector("#paperBtn");
 const scissorBtn = document.querySelector("#scissorBtn");
+const div = document.querySelector(".content");
+const para = document.createElement("p");
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
 
 function getComputerChoice(){
     let choice = Math.ceil(Math.random()*3);
@@ -15,102 +21,117 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(option){
-    //let entry = prompt("Rock Paper or Scissor");
-
     console.log(option);
 
     let choice = option;
 
-    /*if(!(choice === "Rock" || choice === "Paper" || choice === "Scissor")){
-        console.log("Not an option. Try Again");
-        entry = prompt("Rock Paper or Scissor");
-        choice = option.toLowerCase();
-    }*/
-
     return choice;
 }
 
-/*function playGame(){
+function playRound(computerChoice, choice){
 
-    let humanScore = 0;
-    let computerScore = 0;*/
+    let humanChoice = getHumanChoice(choice);
 
-    function playRound(computerChoice, humanChoice){
+    console.log(humanChoice + " " + computerChoice);
 
-        humanChoice = getHumanChoice(humanChoice);
+    if(humanChoice === "Scissor" && computerChoice === "Paper"){
+        humanScore += 1;
+        /*console.log("You Win! Scissor cut paper");
+        console.log("Score: YOU: " + humanScore + " AI: " + computerScore);*/
+        
+        para.textContent = "You Win! Scissor cuts paper";
 
-        console.log(humanChoice + " " + computerChoice);
-    
-        if(humanChoice === "scissor" && computerChoice === "paper"){
-            /*humanScore += 1;
-            console.log("You Win! Scissor cut paper");
-            console.log("Score: YOU: " + humanScore + " AI: " + computerScore);*/
-            const div = document.querySelector(".content");
-            const para = document.createElement("p");
-            
-            para.textContent = "You Win! Scissor cuts paper";
+        div.appendChild(para);
+        round++;
+        
+    }else if(humanChoice === "Paper" && computerChoice === "Rock"){
+        humanScore += 1;
+        /*console.log("You Win! Paper beats rock");
+        console.log("Score: YOU: " + humanScore + " AI: " + computerScore);*/
+        para.textContent = "You Win! Paper beats rock";
 
-            div.appendChild(para);
-            
-        }else if(humanChoice === "paper" && computerChoice === "rock"){
-            /*humanScore += 1;
-            console.log("You Win! Paper beats rock");
-            console.log("Score: YOU: " + humanScore + " AI: " + computerScore);*/
-            const div = document.querySelector(".content");
-            const para = document.createElement("p");
-            
-            para.textContent = "You Win! Paper beats rock";
+        div.appendChild(para);
+        round++;
+    }else if(humanChoice === "Rock" && computerChoice ==="Scissor"){
+        humanScore += 1;
+        /*console.log("You Win! Rock smashes scissor");
+        console.log("Score: YOU: " + humanScore + " AI: " + computerScore);*/
+        para.textContent = "You Win! Rock smashes scissor";
 
-            div.appendChild(para);
-        }else if(humanChoice === "rock" && computerChoice ==="scissor"){
-            /*humanScore += 1;
-            console.log("You Win! Rock smashes scissor");
-            console.log("Score: YOU: " + humanScore + " AI: " + computerScore);*/
-            const div = document.querySelector(".content");
-            const para = document.createElement("p");
-            
-            para.textContent = "You Win! Rock smashes scissor";
+        div.appendChild(para);
+        round++;
+    }else if(humanChoice === computerChoice){
+        //console.log("Play Again");
+        //playRound(getComputerChoice(), getHumanChoice);
+        para.textContent = "Go Again";
 
-            div.appendChild(para);
-        }else if(humanChoice === computerChoice){
-            console.log("Play Again");
-            playRound(getComputerChoice(), getHumanChoice);
-        }else{
-            /*computerScore += 1;
-            console.log("You Lose, " + computerChoice + " beats " + humanChoice);
-            console.log("Score: YOU: " + humanScore + " AI: " + computerScore);*/
+        div.appendChild(para);
+    }else{
+        computerScore += 1;
+        /*console.log("You Lose, " + computerChoice + " beats " + humanChoice);
+        console.log("Score: YOU: " + humanScore + " AI: " + computerScore);*/
+        para.textContent = "You Lose, " + computerChoice + " beats " + humanChoice;
 
-            const div = document.querySelector(".content");
-            const para = document.createElement("p");
-            
-            para.textContent = "You Lose, " + computerChoice + " beats " + humanChoice;
-
-            div.appendChild(para);
-        }
+        div.appendChild(para);
+        round++;
     }
+}
 
+function playGame(choice){
+    playRound(getComputerChoice(), choice);
+
+    let h1 = document.createElement("h1");
+    
     //Temporarily removing 5 rounds to implement UI.
 
     /*for(let i = 1; i <= 5; i++){
         console.log("Round " + i);
-        playRound(getComputerChoice(), getHumanChoice);
-    }
-
-    if(humanScore > computerScore){
-        console.log("YOU WIN!");
-    }else{
-        console.log("YOU LOSE!");
+        //playRound(getComputerChoice(), choice);
     }*/
-//}
+
+    if(round > 5){
+        if(humanScore > computerScore){
+            h1.textContent = "YOU WIN!";
+            div.prepend(h1);
+        }else{
+            h1.textContent = "YOU LOSE!";
+            div.prepend(h1);
+        }
+    }   
+}
 
 // playGame();
 
 rockBtn.addEventListener("click", () => {
-    playRound(getComputerChoice(), rockBtn.textContent)
+    if(round <= 5 ){
+        //playRound(getComputerChoice(), choice);
+        playGame(rockBtn.textContent);
+    }
+
+    if(round > 5){
+        para.textContent = "GAME OVER";
+        div.appendChild(para);
+    }
 });
 paperBtn.addEventListener("click", () => {
-    playRound(getComputerChoice(), paperBtn.textContent)
+    if(round <= 5 ){
+        //playRound(getComputerChoice(), choice);
+        playGame(rockBtn.textContent);
+    }
+    
+    if(round > 5){
+        para.textContent = "GAME OVER";
+        div.appendChild(para);
+    }
 });
 scissorBtn.addEventListener("click", () => {
-    playRound(getComputerChoice(), scissorBtn.textContent)
+    if(round <= 5 ){
+        //playRound(getComputerChoice(), choice);
+        playGame(rockBtn.textContent);
+    }
+    
+    if(round > 5){
+        para.textContent = "GAME OVER";
+        div.appendChild(para);
+    }
 });
